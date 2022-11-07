b21358a5a4307953ab9269087e212f954e41246b
Kenneth Kaiser <kk1995@live.dk>
2022-11-07 17:18:26 +0100

    Created movePlayer and AddPlayer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,23 @@
 package org.example;
 
+import org.Game.Player.Player;
 import org.guiContact.DirectGUICommands;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
         DirectGUICommands guiCommands = new DirectGUICommands();
+        Player player = new Player();
+        guiCommands.AddPlayers(player);
 
-        guiCommands.CreateGUI();
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            scanner.nextLine();
+            guiCommands.MovePlayer(player);
+        }
+
 
 
         
--- src/main/java/org/guiContact/DirectGUICommands.java
+++ src/main/java/org/guiContact/DirectGUICommands.java
@@ -1,13 +1,23 @@
 package org.guiContact;
 
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Player;
 import gui_main.GUI;
+import org.Game.Die.RollDice;
+import org.Game.Player.Player;
+
+import java.awt.*;
 
 public class DirectGUICommands {
     private static GUI gui;
+    private GUI_Player andreas;
+
 
     public DirectGUICommands(){
 
-        gui = getInstance();
+        gui = getInstanceGUI();
+
 
     }
 
@@ -16,19 +26,51 @@ public class DirectGUICommands {
 
     }
 
+    public void MovePlayer(Player player){
 
-    public void AddPlayers(){
+        RollDice rollDice = new RollDice();
 
+        rollDice.Roll();
+        int ourRoll = rollDice.getOurRolls();
+
+        int nextFieldPlacement = player.getPosition() + ourRoll;
+
+        if (nextFieldPlacement > 39){
+            nextFieldPlacement = nextFieldPlacement - 40;
+        }
+
+        GUI_Field fields = gui.getFields()[nextFieldPlacement];
+
+        andreas.getCar().setPosition(fields);
+
+        player.setPosition(nextFieldPlacement);
+        System.out.println(ourRoll);
 
 
     }
 
-    public static GUI getInstance(){
+    public void AddPlayers(Player Cplayer){
+        GUI_Car car = new GUI_Car();
+        car.setPrimaryColor(Color.GREEN);
+        GUI_Player player = new GUI_Player("Andreas",20000,car);
+        gui.addPlayer(player);
+
+        GUI_Field fields = gui.getFields()[0];
+        player.getCar().setPosition(fields);
+
+        Cplayer.setPosition(0);
+
+        this.andreas = player;
+
+    }
+
+    public static GUI getInstanceGUI(){
         if(gui == null){
             gui = new GUI();
         }
         return gui;
     }
+
 
 
 }
