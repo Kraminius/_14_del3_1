9ea41c3c1a354b9dd86708edaea9d50a7d04d348
Kenneth Kaiser <kk1995@live.dk>
2022-11-07 17:17:18 +0100

    Changed rollofDice to ints and not int arrays

--- src/main/java/org/Game/Die/RollDice.java
+++ src/main/java/org/Game/Die/RollDice.java
@@ -4,26 +4,25 @@ import java.util.Random;
 //The base-code is copied from earlier project made by same group. Modified slightly to work in this project.
 public class RollDice {
 
-    private int []ourRolls; //The collected rolls
+    private int ourRoll; //The collected rolls
 
     private final int numberOfRolls = 1; //Number of times the die is being rolled
     private Die die = new Die();
     public void Roll(){
-        ourRolls = new int[numberOfRolls];
         Random random = new Random();
 
-        for (int i = 0;i<numberOfRolls;i++){ //Runs loop a number of times equal to number of rolls.
-            ourRolls[i] = random.nextInt((die.getMaximum()- die.getMinimum())+1)+1; //Rolls the die, and get a random number between min and max
 
-        }
+            ourRoll = random.nextInt((die.getMaximum()- die.getMinimum())+1)+1; //Rolls the die, and get a random number between min and max
+
+
     }
 
-    public int[] getOurRolls() {
-        return ourRolls;
+    public int getOurRolls() {
+        return ourRoll;
     }
 
-    public void setOurRolls(int[] ourRolls) {
-        this.ourRolls = ourRolls;
+    public void setOurRolls(int ourRoll) {
+        this.ourRoll = ourRoll;
     }
 
 }
