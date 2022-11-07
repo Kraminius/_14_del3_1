9c3d7c056431ac9679a8a26a585f5ae080beadfb
Toby <tobiasgoerlyk@hotmail.com>
2022-11-07 16:13:19 +0100

    Move Players Class

--- src/main/java/org/Game/Player/MovePlayers.java
+++ src/main/java/org/Game/Player/MovePlayers.java
@@ -1,4 +1,11 @@
 package org.Game.Player;
 
 public class MovePlayers {
+    int[] playerPositions;
+
+    public MovePlayers(Player[] players){
+        for(int i = 0; i < players.length; i++){
+            players[i].getPosition();
+        }
+    }
 }
---
+++ src/main/java/org/Game/Player/Player.java
@@ -0,0 +1,9 @@
+package org.Game.Player;
+
+public class Player {
+    private int position; //A spot between 1 and 24
+
+    public int getPosition() {
+        return position;
+    }
+}
