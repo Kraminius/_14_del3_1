dbf93c73ee2609b923733880e73444cc344126de
Kenneth Kaiser <kk1995@live.dk>
2022-11-07 16:29:11 +0100

    Can create board and made singleton pattern on GUI object.

--- src/main/java/org/guiContact/DirectGUICommands.java
+++ src/main/java/org/guiContact/DirectGUICommands.java
@@ -1,8 +1,34 @@
 package org.guiContact;
 
+import gui_main.GUI;
+
 public class DirectGUICommands {
-    public void CreateGUI(){
+    private static GUI gui;
+
+    public DirectGUICommands(){
+
+        gui = getInstance();
 
     }
+
+    public void CreateGUI(){
+
+
+    }
+
+
+    public void AddPlayers(){
+
+
+
+    }
+
+    public static GUI getInstance(){
+        if(gui == null){
+            gui = new GUI();
+        }
+        return gui;
+    }
+
 
 }
