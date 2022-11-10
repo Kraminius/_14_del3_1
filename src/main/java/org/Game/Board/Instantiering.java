74c0523d6623567fd48793cb5e4d3e93b4124615
mikkelJurs <s224279@student.dtu.dk>
2022-11-10 11:54:37 +0100

    Changed rest of fields

--- src/main/java/org/Game/Board/Instantiering.java
+++ src/main/java/org/Game/Board/Instantiering.java
@@ -9,8 +9,8 @@ public class Instantiering {
     // Laver Monoply JR boarded
     public GUI_Field[] fields = {
             new GUI_Start("Start", "Modtag M$2, når du passerer", "Modtag M$2, når du passerer", Color.WHITE, Color.BLACK),
-            new GUI_Street("Burgerbaren", "M$1", "", "1", new Color(102, 51, 0), Color.WHITE),
-            new GUI_Street("Pizzariaet", "M$1", "", "1", new Color(102, 51, 0), Color.WHITE),
+            new GUI_Street("Burgerbaren", "M$1", "", "1", new Color(191, 118, 15), Color.BLACK),
+            new GUI_Street("Pizzariaet", "M$1", "", "1", new Color(191, 118, 15), Color.BLACK),
             new GUI_Chance("?", "Chancen", "Træk et chancekort", Color.WHITE, Color.BLACK),
             new GUI_Street("Slikbutikken", "M$1", "", "1", new Color(38, 166, 191), Color.BLACK),
             new GUI_Street("Iskiosken", "M$1", "", "1", new Color(38, 166, 191), Color.BLACK),
@@ -24,14 +24,14 @@ public class Instantiering {
             new GUI_Street("Spillehallen", "M$3", "", "3", new Color(176, 26, 51), Color.BLACK),
             new GUI_Street("Kinoen", "M$3", "", "3", new Color(176, 26, 51), Color.BLACK),
             new GUI_Chance("?", "Chancen", "Træk et chancekort", Color.WHITE, Color.BLACK),
-            new GUI_Street(),
-            new GUI_Street(),
+            new GUI_Street("Legetøjsbutikken", "M$3", "", "3", new Color(235, 252, 40), Color.BLACK),
+            new GUI_Street("Dyrehandlen", "M$3", "", "3", new Color(235, 252, 40), Color.BLACK),
             new GUI_Jail("default", "", "Gå i fængsel", "Gå i fængsel", Color.WHITE, Color.BLACK),
-            new GUI_Street(),
-            new GUI_Street(),
+            new GUI_Street("Bowlinghallen", "M$4", "", "4", new Color(31, 173, 53), Color.BLACK),
+            new GUI_Street("Zoologisk Have", "M$4", "", "4", new Color(31, 173, 53), Color.BLACK),
             new GUI_Chance("?", "Chancen", "Træk et chancekort", Color.WHITE, Color.BLACK),
-            new GUI_Street(),
-            new GUI_Street(),
+            new GUI_Street("Vandlandet", "M$5", "", "5", new Color(50, 75, 166), Color.BLACK),
+            new GUI_Street("Strandpromenaden", "M$5", "", "5", new Color(50, 75, 166), Color.BLACK),
     };
 
 }
