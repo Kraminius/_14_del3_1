8d1120377a74dd8d33c77087da0f95319b28cf62
Toby <tobiasgoerlyk@hotmail.com>
2022-11-10 11:37:45 +0100

    Removed Test

--- src/main/java/org/Game/ChanceCards/Functionality.java
+++ src/main/java/org/Game/ChanceCards/Functionality.java
@@ -6,53 +6,71 @@ import org.guiContact.DirectGUICommands;
 public class Functionality {
     Descriptions desc = new Descriptions();
     DirectGUICommands gui = new DirectGUICommands();
+
     public boolean doFunction(int id, Player player){
         boolean returnCard = true;
+        int position = player.getPosition();
         String[] descriptions = desc.getCardInfo();
         switch (id){
             case 0:
                 //Show the description descriptions[0];
+                //Find navnene på begge felter
+                String navn1 = "";
+                String navn2 = "";
+                switch (gui.TwoChoiceMessage(descriptions[0], navn1, navn2)){
+                    case "1":
+                        //Ryk spilleren til navn1
+                        //Enten betal leje eller få felt uden at betale.
+                        break;
+                    case "2":
+                        //Ryk spilleren til navn2
+                        //Enten betal leje eller få felt uden at betale.
+                        break;
+                    default:
+                        System.out.println("error");
+                        break;
+                }
                 break;
             case 1:
-                //Show the description descriptions[1];
+                //Show the description descriptions[1]; Gratis Orange eller grønt
                 break;
             case 2:
-                //Show the description descriptions[2];
+                //Show the description descriptions[2]; Gratis Lyseblåt
                 break;
             case 3:
-                //Show the description descriptions[3];
+                //Show the description descriptions[3]; Gratis Pink eller mørkeblåt
                 break;
             case 4:
-                //Show the description [4]
+                //Show the description [4] Gratis Rød
                 break;
             case 5:
+                //Ryk til skaterparken, det er gratis
+                break;
+            case 6: //lyseblåt eller rødt gratis
 
                 break;
-            case 6:
+            case 7: //Gratis brunt eller gult
 
                 break;
-            case 7:
-
-                break;
-            case 8:
+            case 8: //Ryk til start
 
                 break;
             case 9:
                 int move = gui.GetIntFromPlayer(descriptions[9] + " Indtast antallet herunder:");
                 if(move >= 0 && move <= 6) {
-                    //Move player (move) amount of steps.
+                    //Move player (move) amount of steps. = position + move
                 }
                 else{
                     while(move < 1 || move > 5){
                         move = gui.GetIntFromPlayer(descriptions[9] + " Dit indtastede tal var udenfor mængden 1-5, prøv igen:");
                     }
-                    //Move player (move) amount of steps.
+                    //Move player (move) amount of steps. = position + move
                 }
                 break;
             case 10:
                 switch (gui.TwoChoiceMessage(descriptions[10], "Ryk 1 felt frem","Tag et Chancekort mere")){
                     case "1":
-                        //Ryk spilleren et felt frem.
+                        //Ryk spilleren et felt frem. = position +1
                         break;
                     case "2":
                         //Pull new card (Needs to be called from the same Class that calls the Cards, otherwise it won't be the same deck and problems will arise.
@@ -63,17 +81,27 @@ public class Functionality {
                 }
 
                 break;
-            case 11:
+            case 11: //Betal 2 penge
+
                 break;
             case 12:
                 //Get out of jail card
                 returnCard = false;
                 break;
-            case 13:
+            case 13: //Ryk til strandpromenaden, ikke gratis
+
                 break;
-            case 14:
+            case 14: //Fødelsdag, modtag 1 fra hver
+                Player[] players = player.getPlayers();
+                for(int i = 0; i<players.length; i++){
+
+                }
+
+
                 break;
-            case 15:
+            case 15: //Modate 2 af banken.
+                gui.messageBox(descriptions[15]);
+                player.setMoney(player.getMoney() + 2);
                 break;
             default:
                 System.out.println("Pulled card index is outside of the array of descriptions and functionality");
