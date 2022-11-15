ce21cef51b2a04a173ef67d88dea2081ce6e4592
mikkelJurs <s224279@student.dtu.dk>
2022-11-16 00:27:20 +0100

    Made constructor in Fields.java, changed all subsequent fields extending Fields.java

--- src/main/java/org/Game/Board/Picker.java
+++ src/main/java/org/Game/Board/Picker.java
@@ -4,15 +4,22 @@ import gui_fields.GUI_Car;
 import gui_fields.GUI_Ownable;
 import gui_fields.GUI_Player;
 import gui_main.GUI;
+import org.Game.Player.Player;
 
 public class Picker {
     private Fields fields;
 
 
-    /* public void LandedField() {
-        switch () {
+    public void landedField(Player player, int fieldIndex) {
+        switch (fieldIndex) {
+            case 1:
+                fields = new Burgerbaren();
+                fields.buyable(player);
+                break;
             default:
 
-     */
+
         }
+    }
+}
 
