fb25578e894c428d49ad7822160790b376405ba5
mikkelJurs <s224279@student.dtu.dk>
2022-11-16 00:26:27 +0100

    Made constructor in Fields.java, changed all subsequent fields extending Fields.java

--- src/main/java/org/Game/Board/Burgerbaren.java
+++ src/main/java/org/Game/Board/Burgerbaren.java
@@ -3,6 +3,10 @@ package org.Game.Board;
 import gui_fields.GUI_Field;
 
 public class Burgerbaren extends Fields {
+    public Burgerbaren() {
+        super(1,1);
+    }
+
     public int buyableFieldID() {
         int fieldID = 1;
 
