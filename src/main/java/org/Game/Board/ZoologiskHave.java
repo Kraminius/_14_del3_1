f0723be15efa590abcf4cb507ce19d296dfbe881
mikkelJurs <s224279@student.dtu.dk>
2022-11-16 00:27:54 +0100

    Made constructor in Fields.java, changed all subsequent fields extending Fields.java

--- src/main/java/org/Game/Board/ZoologiskHave.java
+++ src/main/java/org/Game/Board/ZoologiskHave.java
@@ -1,6 +1,12 @@
 package org.Game.Board;
 
 public class ZoologiskHave extends Fields {
+
+
+    public ZoologiskHave(int Id, int fieldRent) {
+        super(Id, fieldRent);
+    }
+
     public int buyableFieldID() {
         int fieldID = 20;
 
