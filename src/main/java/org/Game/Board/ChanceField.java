01be05d98281925e4567e31c469a6d54f16e9e1e
mikkelJurs <s224279@student.dtu.dk>
2022-11-16 00:26:33 +0100

    Made constructor in Fields.java, changed all subsequent fields extending Fields.java

--- src/main/java/org/Game/Board/ChanceField.java
+++ src/main/java/org/Game/Board/ChanceField.java
@@ -2,6 +2,11 @@ package org.Game.Board;
 
 public class ChanceField extends Fields {
 
+
+    public ChanceField(int Id, int fieldRent) {
+        super(Id, fieldRent);
+    }
+
     public int buyableFieldID() {
         int fieldID = 0;
 
