52378b903986878a25b0cd695083ca3086f7e2ca
mikkelJurs <s224279@student.dtu.dk>
2022-11-16 00:26:38 +0100

    Made constructor in Fields.java, changed all subsequent fields extending Fields.java

--- src/main/java/org/Game/Board/Dyrehandlen.java
+++ src/main/java/org/Game/Board/Dyrehandlen.java
@@ -1,6 +1,11 @@
 package org.Game.Board;
 
 public class Dyrehandlen extends Fields {
+
+    public Dyrehandlen(int Id, int fieldRent) {
+        super(Id, fieldRent);
+    }
+
     public int buyableFieldID() {
         int fieldID = 17;
 
