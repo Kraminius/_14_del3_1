96ad2d2085a8bde4d11feedb28d933e3f5e261ea
mikkelJurs <s224279@student.dtu.dk>
2022-11-16 00:26:58 +0100

    Made constructor in Fields.java, changed all subsequent fields extending Fields.java

--- src/main/java/org/Game/Board/Iskiosken.java
+++ src/main/java/org/Game/Board/Iskiosken.java
@@ -1,6 +1,12 @@
 package org.Game.Board;
 
 public class Iskiosken extends Fields {
+
+
+    public Iskiosken(int Id, int fieldRent) {
+        super(Id, fieldRent);
+    }
+
     public int buyableFieldID() {
         int fieldID = 5;
 
