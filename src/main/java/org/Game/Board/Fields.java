0fe800311d4e3969af3130884288ea8553a50217
mikkelJurs <s224279@student.dtu.dk>
2022-11-16 00:29:31 +0100

    Added method to buy a property if landed on free field.

--- src/main/java/org/Game/Board/Fields.java
+++ src/main/java/org/Game/Board/Fields.java
@@ -1,7 +1,42 @@
 package org.Game.Board;
 
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Ownable;
+import gui_main.GUI;
+import org.Game.Player.Player;
+import org.guiContact.DirectGUICommands;
+
 public abstract class Fields {
     int fieldID;
-    String fieldDescription;
+    private int fieldRent;
+
+
+    Player player;
+    Instantiering instantiering = new Instantiering();
+
+    public Fields(int Id, int fieldRent){
+        this.fieldID = Id;
+        this.fieldRent = fieldRent;
+    }
+
     public abstract int buyableFieldID();
+
+    public void buyable(Player player) {
+        if (this.player == null) {
+            isPlayerLoadedOrNot(player);
+        }
+
+    }
+    public void buyProperty(Player player) {
+        this.player = player;
+        DirectGUICommands directGUICommands = DirectGUICommands.getInstance();
+        directGUICommands.ownedPropertyGUI(player, fieldID);
+    }
+
+    public void isPlayerLoadedOrNot(Player player) {
+        if (player.getMoney() >= fieldRent) {
+            buyProperty(player);
+        }
+
+    }
 }
