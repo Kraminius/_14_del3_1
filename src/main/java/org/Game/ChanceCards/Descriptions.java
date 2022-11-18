262a6474c43643c542b37a154588bb69f3edc8b8
mikkelJurs <s224279@student.dtu.dk>
2022-11-18 11:19:03 +0100

    Changed method for Descriptions, changed it so it reads a file, and prints out a data[], so we can call method and array from elsewhere.

--- src/main/java/org/Game/ChanceCards/Descriptions.java
+++ src/main/java/org/Game/ChanceCards/Descriptions.java
@@ -1,41 +1,33 @@
 package org.Game.ChanceCards;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Descriptions {
-
-    private String[] descriptions = new String[20];
-    public Descriptions(){
-        descriptions[0] = "Gratis felt! Ryk frem til et orange felt." +
-                " Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.";
-        descriptions[1] = "Gratis felt! Ryk frem til et orange eller grønt felt." +
-                " Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.";
-        descriptions[2] = "Gratis felt! Ryk frem til et lyseblåt felt." +
-                " Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.";
-        descriptions[3] = "Gratis felt! Ryk frem til et pink eller mørkeblåt felt." +
-                " Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.";
-        descriptions[4] = "Gratis felt! Ryk frem til et rødt felt." +
-                " Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.";
-        descriptions[5] = "Gratis felt! Ryk frem til Skateparken for at lave det perfekt grind!" +
-                " Hvis ingen ejer den, får du den gratis! Eller skal du betale leje til ejeren.";
-        descriptions[6] = "Gratis felt! Ryk frem til et lyseblåt eller rødt felt." +
-                " Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren";
-        descriptions[7] = "Gratis felt! Ryk frem til et brunt eller gult felt." +
-                " Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.";
-        descriptions[8] = "Ryk frem til start. Modtag 2 penge";
-        descriptions[9] = "Ryk op til 5 felter frem.";
-        descriptions[10] = "Ryk 1 felt frem, eller tag et chancekort mere.";
-        descriptions[11] = "Du har spist for meget slik. Betal 2 penge til banken.";
-        descriptions[12] = "Du løslades uden omkostninger." +
-                " Behold dette kort, indtil du får brug for det.";
-        descriptions[13] = "Ryk frem til Strandpromenaden.";
-        descriptions[14] = "Det er din fødselsdag! Alle giver dig 1 penge. TILLYKKE MED FØDSELSDAGEN!";
-        descriptions[15] = "Du har lavet alle dine lektier! " +
-                "Modtag 2 penge fra banken.";
-
+    public String fileReader(int index){
+        String[] data = ReadCol(1,"src/main/java/org/Game/ChanceCards/ChanceCardText.csv", ";");
+        return data[index];
     }
+    public static String[] ReadCol(int col, String filepath, String delimiter) {
+        String data[];
+        String currentLine;
+        ArrayList<String> colData = new ArrayList<>();
 
-    public String[] getCardInfo(){
-        return descriptions;
+        try
+        {
+            FileReader read = new FileReader(filepath);
+            BufferedReader bread = new BufferedReader(read);
+            while((currentLine = bread.readLine()) != null)
+            {
+                data = currentLine.split(delimiter);
+                colData.add(data[col]);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+        return colData.toArray(new String[0]);
     }
-
-
 }
