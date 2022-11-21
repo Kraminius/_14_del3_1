9eeade76a92dbd9268bd8ce51e06b0b5537228ce
mikkelJurs <s224279@dtu.dk>
2022-11-21 19:16:05 +0100

    Testi test

--- src/main/java/org/Game/GameLogic.java
+++ src/main/java/org/Game/GameLogic.java
@@ -140,11 +140,9 @@ public class GameLogic {
                 if(player.getPlayers()[i].getMoney()>player.getPlayers()[j].getMoney()){
                     temp = player.getPlayers()[i];
                     player.getPlayers()[i] = player.getPlayers()[j];
-
                     player.getPlayers()[j] = temp;
                 }
             }
-
         }
 
         temp = player.getPlayers()[player.getPlayers().length-1];
