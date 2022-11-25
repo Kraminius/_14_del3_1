a923ee682576fec5ae353b4c9d49c328ae1e1622
mikkelJurs <s224279@dtu.dk>
2022-11-25 17:15:29 +0100

    Changed die to rafflecup as Trellobard asked for that changed. Deleted Die and made the variables in Rafflecup.java class.

--- src/main/java/org/Game/Die/Rafflecup.java
+++ src/main/java/org/Game/Die/Rafflecup.java
@@ -2,18 +2,18 @@ package org.Game.Die;
 
 import java.util.Random;
 //The base-code is copied from earlier project made by same group. Modified slightly to work in this project.
-public class RollDice {
+public class Rafflecup {
+
+    private final int MAXIMUM = 6;
+    private final int MINIMUM = 1;
 
     private int ourRoll; //The collected rolls
 
     private final int numberOfRolls = 1; //Number of times the die is being rolled
-    private Die die = new Die();
     public void Roll(){
         Random random = new Random();
 
-
-            ourRoll = random.nextInt((die.getMaximum()- die.getMinimum())+1)+1; //Rolls the die, and get a random number between min and max
-
+            ourRoll = random.nextInt((MAXIMUM-MINIMUM)+1)+1; //Rolls the die, and get a random number between min and max
 
     }
 
